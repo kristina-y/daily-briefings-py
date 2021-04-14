ee7992c0ced100c329a2679ac870fa175602bb41
Kristina Yarovinsky <ky226@georgetown.edu>
2021-04-14 17:37:13 -0400

    Weather form

--- web_app/routes/weather_routes.py
+++ web_app/routes/weather_routes.py
@@ -8,14 +8,29 @@ weather_routes = Blueprint("weather_routes", __name__)
 
 @weather_routes.route("/weather/forecast.json")
 def weather_forecast_api():
-    print("WEATHER FORECAST (API)...")
-    print("URL PARAMS:", dict(request.args))
+@weather_routes.route("/weather/form")
+def weather_form():
+    print("WEATHER FORM...")
+    return render_template("weather_form.html")
 
-    country_code = request.args.get("country_code") or "US"
-    zip_code = request.args.get("zip_code") or "20057"
+@weather_routes.route("/weather/forecast", methods=["GET", "POST"])
+def weather_forecast():
+    print("WEATHER FORECAST...")
+
+    if request.method == "GET":
+        print("URL PARAMS:", dict(request.args))
+        request_data = dict(request.args)
+    elif request.method == "POST": # the form will send a POST
+        print("FORM DATA:", dict(request.form))
+        request_data = dict(request.form)
+
+    country_code = request_data.get("country_code") or "US"
+    zip_code = request_data.get("zip_code") or "20057"
 
     results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
     if results:
-        return jsonify(results)
+        #flash(f"Weather Forecast Generated Successfully!", "success")
+        return render_template("weather_forecast.html", country_code=country_code, zip_code=zip_code, results=results)
     else:
-        return jsonify({"message":"Invalid Geography. Please try again."}), 404
+        #flash(f"Geography Error. Please try again!", "danger")
+        return redirect("/weather/form")
