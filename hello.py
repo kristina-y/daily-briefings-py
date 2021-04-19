b69b02f2687cd323dd31d7a5cc84676f77318608
Kristina Yarovinsky <ky226@georgetown.edu>
2021-04-18 21:46:24 -0400

    First 4 checkpoints complete

--- hello.py
+++ hello.py
@@ -1,8 +1,67 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+#
+# HOME ROUTES
+#
 
+@app.route("/")
+@app.route("/home")
+def index():
+    print("HOME...")
+    return "Welcome Home"
+
+@app.route("/about")
+def about():
+    print("ABOUT...")
+    return "About Me"
+
+@app.route("/hello")
 def hello_world():
-    return 'Hello, World!'
+    print("HELLO...", dict(request.args))
+    # NOTE: `request.args` is dict-like, so below we're using the dictionary's `get()` method,
+    # ... which will return None instead of throwing an error if key is not present
+    # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
+    name = request.args.get("name") or "World"
+    return f"Hello, {name}!"
+
+#
+# BOOK ROUTES
+#
+
+@app.route("/api/books")
+@app.route("/api/books.json")
+def list_books():
+    print("BOOKS...")
+    books = [
+        {"id": 1, "title": "Book 1", "year": 1957},
+        {"id": 2, "title": "Book 2", "year": 1990},
+        {"id": 3, "title": "Book 3", "year": 2031},
+    ] # some dummy / placeholder data
+    return jsonify(books)
+
+@app.route("/api/books/<int:book_id>")
+@app.route("/api/books/<int:book_id>.json")
+def get_book(book_id):
+    print("BOOK...", book_id)
+    book = {"id": book_id, "title": f"Example Book", "year": 2000} # some dummy / placeholder data
+    return jsonify(book)
+
+#
+# WEATHER ROUTES
+#
+
+@app.route("/weather/forecast.json")
+def weather_forecast_api():
+    print("WEATHER FORECAST (API)...")
+    print("URL PARAMS:", dict(request.args))
+
+    country_code = request.args.get("country_code") or "US"
+    zip_code = request.args.get("zip_code") or "20057"
+
+    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
+    if results:
+        return jsonify(results)
+    else:
+        return jsonify({"message":"Invalid Geography. Please try again."}), 404
